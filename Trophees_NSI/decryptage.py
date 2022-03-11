ee70022976c0ff9ce4c74fb07e216eec6907e252
Antouaane <88931612+Antouaane@users.noreply.github.com>
2022-03-11 21:30:22 +0100

    Add files via upload

---
+++ Trophees_NSI/decryptage.py
@@ -0,0 +1,7 @@
+# il faudrait une fonction 'decrypt(msg)' 
+# comment on peut s'y prendre ? 
+    # un programme qui convertie les codes en messages :
+        # un programme pour les symbôles
+        # un programme pour les nombres 
+        # un programme pour les lettres 
+        
